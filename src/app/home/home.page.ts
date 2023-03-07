9a74b8418891313b1fda7212393ba15be0116bc0
Ricardo Caballero <ricpar123@gmail.com>
2023-03-07 09:04:59 -0300

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Geolocation, Position } from '@capacitor/geolocation';
+import { Share } from '@capacitor/share';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +10,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+ position: Position | undefined
+
   constructor() {}
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+   this.position = coordinates;
+  }
+
+  async share() {
+    await Share.share({
+      title: 'Come and find me',
+      text: `Here's my current location: 
+        ${this.position?.coords.latitude}, 
+        ${this.position?.coords.longitude}`,
+      url: 'http://ionicacademy.com/'
+    });
+  }
 
 }
